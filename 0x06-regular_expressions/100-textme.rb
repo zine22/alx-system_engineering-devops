81e3b17fdc2a76b6fe3a257693b92a28cd5d57da
zine22 <kamiljamil15@gmail.comgit config --global user.name zine22>
2024-02-27 07:17:20 -0800

    reg

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Make a script about textme
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
