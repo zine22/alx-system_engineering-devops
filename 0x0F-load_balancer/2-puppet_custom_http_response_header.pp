59b5cf7d5852cc869dcea1038fa06424288dfc2d
zine22 <kamiljamil15@gmail.com>
2024-07-08 16:05:08 -0700

    load

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+# manifest to configure custom responses headers from nginx
+
+exec { 'update':
+    command => '/usr/bin/apt-get update -y',
+}
+
+package { 'nginx':
+    ensure  => installed,
+    require => Exec['update']
+}
+
+file_line {'addHeader':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen 80 default_server;',
+    line    => 'add_header X-Served-By $HOSTNAME;',
+    require => Package['nginx'],
+}
+
+service {'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+}
---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,1 @@
+dir for load balancer
